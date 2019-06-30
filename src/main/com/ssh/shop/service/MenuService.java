9725213fc9024fe8c1354d0daf7163554043e28b
unknown <2016012023@protonmail.com>
2019-06-30 14:23:41 +0800

    删除

--- src/main/com/ssh/shop/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package main.com.ssh.shop.service;
-
-import main.com.ssh.shop.entity.Menu;
-
-import java.util.List;
-
-public interface MenuService extends BaseService<Menu> {
-    public List<Menu> getAllMenu();
-}
